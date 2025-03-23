44247c7567ebb35b3bc55ea369b5ae6584d084fb
sara-soomro <soomro.sara0@gmail.com>
2025-03-23 12:49:19 -0700

    Update Create_DS.sql

--- Create_DS.sql
+++ Create_DS.sql
@@ -4,6 +4,13 @@ OPTIONS (
   uris = ['gs://buc-inspiring-453522_m5/Walmart_customer_purchases.parquet/part-*.parquet']
 );
 
+
+/* 
+  External table is created in BIG QUERY via Parquet files stored on GCS .  
+  Partitioned table is made partitioning data based on Purchase Date column 
+*/
+
+  
 CREATE OR REPLACE TABLE `inspiring-code-453522-m5.soomro_zoomcamp_DS.Walmart_customer_purchases_partitioned`
 PARTITION BY DATE(Purchase_Date)
 AS
